e9f7329bdfe736bec1582c0999e46d8d6d439c80
felix0351 <felix0351z@gmail.com>
2022-11-12 18:22:08 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use cpal::{BuildStreamError, SampleFormat, Stream, StreamConfig, StreamError};
+use cpal::{BufferSize, BuildStreamError, SampleFormat, SampleRate, Stream, StreamConfig, StreamError, SupportedBufferSize};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
 fn main() {
@@ -10,16 +10,32 @@ fn main() {
     let device = host.default_output_device().unwrap();
     let audio_config = device.default_output_config().expect("Failed to load audio config");
 
+    println!("Sample Rate: {}", audio_config.config().sample_rate.0);
+    println!("Amount channels: {}", audio_config.config().channels);
+
+   match audio_config.buffer_size() {
+        SupportedBufferSize::Range { min, max } => {
+            println!("Buffer Range: {}-{}", min, max)
+        }
+        SupportedBufferSize::Unknown => {
+            println!("Buffer range unknown")
+        }
+    }
+
     let stream = device.build_input_stream(
-        &audio_config.config(),
+        &StreamConfig {
+            channels: 2,
+            sample_rate: SampleRate(48000),
+            buffer_size: BufferSize::Default
+        },
         move |data: &[f32], error: &cpal::InputCallbackInfo| {
-        println!("{:?}", data)
+            println!("{:?}", data);
+            println!("Length: {}", data.len());
     }, move |error: StreamError| {
-        println!("Error")
+            println!("Error")
     }).expect("Failed to create stream");
 
     stream.play().expect("Failed to play stream");
-
     loop {
 
     }
