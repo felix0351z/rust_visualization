bb74501585b10c05e772cb85c8f2c9373474360d
felix <felix0351z@gmail.com>
2022-11-29 17:59:09 +0100

    Added effect and filter handling to the engine

--- src/engine.rs
+++ src/engine.rs
@@ -9,18 +9,30 @@ mod processing;
 
 use input::*;
 use utils::AudioBuffer;
-use errors::InputError;
+use errors::{ProgramError, InputError};
+use effects::{EffectProcessing, Effect, EffectInfo};
+use effects::frequency::FrequencyEffect;
+use filters::{FilterProcessing, Filter, FilterInfo};
 
-use error_stack::Result;
+use error_stack::{Report, Result, ResultExt};
 use cpal::{InputCallbackInfo};
+use crate::engine::utils::Domain;
 
 
-
-/// Direct interaction with
 pub struct Engine {
     input: DeviceInputSource,
-
     // send handler
+
+    //Led amount of the device
+    n_led: usize,
+
+    // All available effects and filters
+    effects: Vec<Effect>,
+    filters: Vec<Filter>,
+
+    filtering: bool,
+    current_effect: usize,
+    current_filter: usize,
 }
 
 
@@ -28,11 +40,30 @@ impl Engine {
 
 
     /// Generates a new engine with all necessary dependencies
-    pub fn new() -> Engine {
+    pub fn new(n_led: usize) -> Engine {
         let input = DeviceInputSource::new();
 
+        let effects: Vec<Effect> = vec![
+            Effect::new(
+                "Frequency Effect",
+                "/fdgfd",
+                Domain::FrequencyDomain,
+                Box::new(FrequencyEffect)
+            )
+
+
+
+        ];
+        let filters: Vec<Filter> = vec![];
+
         Engine {
-            input
+            input,
+            n_led,
+            effects,
+            filters,
+            filtering: false,
+            current_effect: 0,
+            current_filter: 0
         }
     }
 
@@ -44,8 +75,9 @@ impl Engine {
     }
 
     /// Set a specific device as data input
-    pub fn set_device(&mut self, position: usize) -> Result<(), InputError> {
-        self.input.set_device(position)?;
+    pub fn set_device(&mut self, position: usize) -> Result<(), ProgramError> {
+        self.input.set_device(position)
+            .change_context(ProgramError::InputError)?;
 
         // Update the stream after the device was changed
         self.update_stream()
@@ -62,49 +94,116 @@ impl Engine {
         self.input.pause_stream()
     }
 
-    pub fn get_effects(&self) {
-        todo!()
+    /// Get all available effects
+    pub fn get_effects(&self) -> Vec<EffectInfo> {
+        let mut list: Vec<EffectInfo> = vec![];
+
+        for effect in self.effects.iter() {
+            list.push(effect.get_info());
+        }
+
+        list
     }
 
-    pub fn set_effect(&mut self, position: usize) -> Result<(), InputError> {
-        ///....
+    /// Get all available filters
+    pub fn get_filters(&self) -> Vec<FilterInfo> {
+        let mut list: Vec<FilterInfo> = vec![];
+
+        for filter in self.filters.iter() {
+            list.push(filter.get_info());
+        }
+
+        list
+    }
+
+    /// Set the current effect
+    pub fn set_effect(&mut self, position: usize) -> Result<(), ProgramError> {
+        self.current_effect = position;
 
         self.update_stream()
+    }
+
+    // Set the current filter
+    pub fn set_filter(&mut self, position: usize) -> Result<(), ProgramError> {
+        self.current_filter = position;
+
+        self.update_stream()
+    }
+
+    pub fn is_filtering_activated(&self) -> bool {
+        self.filtering
+    }
+
+    pub fn set_filtering(&mut self, value: bool) {
+        self.filtering = value
     }
 
 
     //---------------------Private-Methods---------------------------------
 
     /// Drops the current stream and start a new stream
-    fn update_stream(&mut self) -> Result<(), InputError> {
+    fn update_stream(&mut self) -> Result<(), ProgramError> {
         //self.pause_stream()?;
         self.build_stream()?;
 
-
         self.start_stream()
+            .change_context(ProgramError::InputError)
+    }
+
+    fn get_current_effect(&self) -> Result<&Effect, ProgramError> {
+        self.effects.get(self.current_effect)
+            .ok_or(
+                Report::from(ProgramError::EffectNotFound(self.current_effect))
+            )
+    }
+
+    fn get_current_filter(&self) -> Option<&Filter> {
+        if self.filtering { None? }
+
+        self.filters.get(self.current_filter)
+    }
+
+    fn get_frame_length(&self) -> Result<usize, ProgramError> {
+        let info = self.input.buffer_info()
+            .change_context(ProgramError::InputError)?;
+
+        Ok(info.frame_length)
     }
 
 
-    fn build_stream(&mut self) -> Result<(), InputError> {
+    fn build_stream(&mut self) -> Result<(), ProgramError> {
 
-        // The input contains 1 buffer and for framing we want so save 2 frames.
-        let frame_length = self.input.buffer_info()?.buffer_size();
-        let buffer = AudioBuffer::new(frame_length, 2);
+        let frame_length = self.get_frame_length()?;
+        let effect = self.get_current_effect()?.create();
+        let filter = match self.get_current_filter() {
+            Some(value) => Some(value.create()),
+            None => None
+        };
 
+        // Define callback
         let call =  |data: &[f32]| {
             println!("First: {:?}", data[0]);
         };
-        let mut worker = Worker::new(buffer, call);
 
-        self.input.build_mono_stream(
-            move |data, info| {
-                worker.process(data, info)
-            },
-            move |err| {
-                todo!()
-            }
+        {
+            //Build the worker & stream
+            let mut worker = Worker::new(
+                call, frame_length,
+                self.n_led, effect, filter);
 
-        )?;
+            self.input.build_mono_stream(
+                move |data, info| {
+
+                    worker.process(data, info)
+
+                },
+                move |err| {
+                    todo!()
+                }
+
+            )
+                .change_context(ProgramError::InputError)?;
+        }
 
         Ok(())
     }
@@ -113,6 +212,8 @@ impl Engine {
 }
 
 
+//TODO: 1. Framing, 2. Effect, 3. Domain, 4. Filter, N_FFT, N_Melbank, N_LEDs
+//TODO:  Hat: Raw audio buffer
 
 
 
@@ -125,10 +226,19 @@ pub struct Worker<C>
     // implemented the Send Trait.
         C: FnMut(&[f32]) + Send + 'static,
 {
+    //Input and Output
     callback: C,
-    buffer: AudioBuffer<f32>,
-    //effect
-    //filter
+    last_frame: AudioBuffer<f32>,
+    fft_buffer: [f32; processing::N_FFT],
+    //mel buffer -> heap
+    //effect buffer -> heap
+
+    n_led: usize,
+
+    //Framing factor
+    effect: Box<dyn EffectProcessing + Send>,
+    filter: Option<Box<dyn FilterProcessing + Send>>
+
 }
 
 
@@ -137,9 +247,23 @@ where
     C: FnMut(&[f32]) + Send + 'static
 {
 
-    /// Generates a new Worker struct with an callback
-    fn new(buffer: AudioBuffer<f32>, callback: C) -> Self {
-        Worker { callback, buffer }
+    /// Generates a new Worker struct
+    fn new(
+        callback: C,
+        frame_length: usize,
+        n_led: usize,
+        effect: Box<dyn EffectProcessing + Send>,
+        filter: Option<Box<dyn FilterProcessing + Send>>
+    ) -> Self {
+        let last_frame = AudioBuffer::new(frame_length, 1);
+        let fft_buffer = [0.0 as f32; processing::N_FFT];
+
+
+
+        Worker {
+            callback, last_frame, fft_buffer,
+            n_led, effect, filter
+        }
     }
 
     /// Function which consumes the raw input data and process the effect
@@ -147,6 +271,12 @@ where
         //....
 
         (self.callback)(data)
+    }
+
+
+    /// Process the pre emphasis over the input signal
+    fn pre_emphasis(&mut self) {
+
     }
 
 }
